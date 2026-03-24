20d5e5d00e95d37c8694f3c810c5bdd4e44ca81e
Sami Munir <samidmunir@outlook.com>
2026-03-24 03:23:47 -0400

    refactoring DashboardPage to extend BasePage

--- jira-test-framework/src/test/java/com/sami/jira/ui/pages/DashboardPage.java
+++ jira-test-framework/src/test/java/com/sami/jira/ui/pages/DashboardPage.java
@@ -1,38 +1,27 @@
 package com.sami.jira.ui.pages;
 
+import java.util.concurrent.TimeoutException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class DashboardPage {
-    private WebDriver driver;
+import com.sami.jira.base.BasePage;
+
+public class DashboardPage extends BasePage {
+    private By dashboardHeader = By.xpath("//h1[text()='System Dashboard']");
+    private By adminSettingsDropdown = By.id("admin_menu");
+    private By userManagementOption = By.id("admin_users_menu");
 
     public DashboardPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
-    private By dashboardHeaderBy = By.xpath("//h1[text()='System Dashboard']");
-    private By adminSettingsBy = By.id("admin_menu");
-    private By userManagementOptionBy = By.id("admin_users_menu");
-
-    public boolean isDashboardPageDisplayed() {
-        return driver.findElement(dashboardHeaderBy).isDisplayed();
+    public boolean isDisplayed() throws TimeoutException {
+        return isDisplayed(dashboardHeader);
     }
 
-    public void goToUserManagementPage() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        driver.findElement(adminSettingsBy).click();
-        
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        driver.findElement(userManagementOptionBy).click();
+    public void goToUserManagement() {
+        clickWithFallback(adminSettingsDropdown);
+        clickWithFallback(userManagementOption);
     }
 }
