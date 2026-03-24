c1a45fd860b00c5b994de9d1b79dd6d12a3d2474
Sami Munir <samidmunir@outlook.com>
2026-03-24 00:14:13 -0400

    adding dep for WebDriverManager, implementing BaseUITest class

---
+++ jira-test-framework/src/test/java/com/sami/jira/base/BaseUITest.java
@@ -0,0 +1,31 @@
+package com.sami.jira.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseUITest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void init() {
+        WebDriverManager.chromedriver().setup();
+        
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("http://localhost:8080");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
